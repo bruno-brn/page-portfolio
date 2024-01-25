663365e8a145f128f5b8c211f3f2d3b62a1d7030
bruno-brn <123398572+bruno-brn@users.noreply.github.com>
2024-01-25 17:51:17 -0300

    ajuste na responsividade

--- menu.js
+++ menu.js
@@ -1,15 +1,39 @@
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
-let orverlay = document.getElementById("orverlay-menu")
+let overlay = document.getElementById("overlay-menu")
+let dropdown = document.getElementById('dropdown');
 
-btnMenu.addEventListener('click', () =>{
+btnMenu.addEventListener('click', () => {
     menu.classList.add('abrir-menu')
+    overlay.style.display = 'block' // Mostra o overlay
 })
 
-menu.addEventListener('click', () =>{
+menu.addEventListener('click', () => {
     menu.classList.remove('abrir-menu')
 })
 
-orverlay.addEventListener('click', () =>{
+overlay.addEventListener('click', () => {
     menu.classList.remove('abrir-menu')
+    overlay.style.display = 'none' // Esconde o overlay
 })
+
+dropdown.addEventListener('click', function(event) {
+    event.stopPropagation();
+    let dropdownContent = document.getElementById('dropdown-content');
+    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+});
+
+// Fechar o submenu quando clicar fora
+window.addEventListener('click', function(event) {
+    let dropdownContent = document.getElementById('dropdown-content');
+    if (dropdownContent.style.display === 'block') {
+        dropdownContent.style.display = 'none';
+    }
+});
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+    overlay.style.display = 'none' // Esconde o overlay
+});
+
+
