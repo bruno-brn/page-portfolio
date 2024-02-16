41abc2e4e80764997a18a9044a4442ef1ae6faf9
bruno-brn <123398572+bruno-brn@users.noreply.github.com>
2024-02-16 11:05:34 -0300

    ajustes no html e css

--- menu.js
+++ menu.js
@@ -1,39 +1,53 @@
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
-let overlay = document.getElementById("overlay-menu")
-let dropdown = document.getElementById('dropdown');
+let overlay = document.getElementById('overlay-menu')
+let dropdown = document.getElementById('dropdown')
 
 btnMenu.addEventListener('click', () => {
-    menu.classList.add('abrir-menu')
-    overlay.style.display = 'block' // Mostra o overlay
+  menu.classList.add('abrir-menu')
+  overlay.style.display = 'block' // Mostra o overlay
 })
 
 menu.addEventListener('click', () => {
-    menu.classList.remove('abrir-menu')
+  menu.classList.remove('abrir-menu')
 })
 
 overlay.addEventListener('click', () => {
-    menu.classList.remove('abrir-menu')
-    overlay.style.display = 'none' // Esconde o overlay
+  menu.classList.remove('abrir-menu')
+  overlay.style.display = 'none' // Esconde o overlay
 })
 
-dropdown.addEventListener('click', function(event) {
-    event.stopPropagation();
-    let dropdownContent = document.getElementById('dropdown-content');
-    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
-});
+dropdown.addEventListener('click', function (event) {
+  event.stopPropagation()
+  let dropdownContent = document.getElementById('dropdown-content')
+  dropdownContent.style.display =
+    dropdownContent.style.display === 'block' ? 'none' : 'block'
+})
 
 // Fechar o submenu quando clicar fora
-window.addEventListener('click', function(event) {
-    let dropdownContent = document.getElementById('dropdown-content');
-    if (dropdownContent.style.display === 'block') {
-        dropdownContent.style.display = 'none';
-    }
-});
+window.addEventListener('click', function (event) {
+  let dropdownContent = document.getElementById('dropdown-content')
+  if (dropdownContent.style.display === 'block') {
+    dropdownContent.style.display = 'none'
+  }
+})
 
 menu.addEventListener('click', () => {
-    menu.classList.remove('abrir-menu')
-    overlay.style.display = 'none' // Esconde o overlay
-});
+  menu.classList.remove('abrir-menu')
+  overlay.style.display = 'none' // Esconde o overlay
+})
 
+const menuLinks = document.querySelectorAll('.scroll-link')
 
+menuLinks.forEach((link) => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault()
+    const targetId = this.getAttribute('href')
+    const targetElement = document.querySelector(targetId)
+
+    targetElement.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+  })
+})
